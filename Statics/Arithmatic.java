9815a2946d67d49bcf40ca9c1033cce337097850
sameer0309 <pande_ss@hotmail.com>
2015-02-13 13:03:58 -0800

    Arithmatic functions

---
+++ Statics/Arithmatic.java
@@ -0,0 +1,22 @@
+package Statics;
+
+public class Arithmatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int add = addition(5,7);
+		System.out.print("addition is : " + add);
+
+	}
+	
+	public static int addition (int i , int j)
+	{
+		int k =0;
+		k = i + j;
+		
+		return k;
+		
+	}
+
+}
